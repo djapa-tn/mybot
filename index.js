6a5ce7540ca4035f8447917abfeea22f18f860f8
djapa-tn <86630464+djapa-tn@users.noreply.github.com>
2026-01-09 12:23:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const express = require("express");
 const app = express();
 
+// إنشاء البوت مع النوايا المطلوبة
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
+// Express Web Server
 app.get("/", (req, res) => {
   res.send("Bot is running...");
 });
@@ -14,19 +16,18 @@ app.listen(3000, () => {
   console.log("Web server started");
 });
 
-client.once("clientReady", () => {
+// حدث تسجيل الدخول للبوت
+client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
   client.user.setActivity("TikTok Skits", { type: "WATCHING" });
 });
 
-
-
+// أوامر البوت
 client.on("messageCreate", msg => {
   if (msg.content === "IP") {
     msg.reply("SOON");
   }
 });
 
-
+// تسجيل الدخول باستخدام التوكن
 client.login(process.env.TOKEN);
-
